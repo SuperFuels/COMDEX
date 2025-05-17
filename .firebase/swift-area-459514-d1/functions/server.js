082443e298ecc3b3802d813bc3fd9ee3c1901bc6
SuperFuels <kevin@super-fuels.com>
2025-05-17 21:01:18 +0200

    Your descriptive commit message here

---
+++ .firebase/swift-area-459514-d1/functions/server.js
@@ -0,0 +1,4 @@
+const { onRequest } = require('firebase-functions/v2/https');
+  const server = import('firebase-frameworks');
+  exports.ssrswiftarea459514d1 = onRequest({}, (req, res) => server.then(it => it.handle(req, res)));
+  
