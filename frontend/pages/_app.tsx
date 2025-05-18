2f8b86395d75fdf8a3baf6f9d476476ef1dfb50d
SuperFuels <kevin@super-fuels.com>
2025-05-18 18:31:05 +0200

    🔧 Update env files to point to production API and add client‐side logging in _app.tsx

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,10 +1,15 @@
 // pages/_app.tsx
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
 import Navbar from '../components/Navbar'
 import SwapBar from '../components/SwapBar'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    console.log('🔍 NEXT_PUBLIC_API_URL =', process.env.NEXT_PUBLIC_API_URL)
+  }, [])
+
   return (
     <>
       {/* global navbar */}
