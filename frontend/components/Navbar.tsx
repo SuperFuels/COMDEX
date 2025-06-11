92c8db99010dede598df3e6b7d8037ba401b57a9
peekoo123 <peekoo123@googlemail.com>
2025-06-11 21:41:24 +0000

    chore: deploy 🚀

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link'
 import Image from 'next/image'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 import { useEffect, useState, useRef, useCallback } from 'react'
 import api from '@/lib/api'
 import { UserRole } from '@/hooks/useAuthRedirect'
