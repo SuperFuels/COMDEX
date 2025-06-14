83f485e3d2c3a777bf012082edad0b043999a58f
peekoo123 <peekoo123@googlemail.com>
2025-06-14 20:36:27 +0000

    chore: deploy 🚀

--- frontend/pages/api/shipments.js
+++ frontend/pages/api/shipments.js
@@ -1,5 +1,8 @@
 export default async function handler(req, res) {
-  const apiRes = await fetch(\`\${process.env.BACKEND_URL}/shipments\`)
-  const data = await apiRes.json()
-  res.status(apiRes.status).json(data)
+  // forward to your backend
+  const apiRes = await fetch(
+    `${process.env.BACKEND_URL}/shipments`
+  );
+  const data = await apiRes.json();
+  return res.status(apiRes.status).json(data);
 }
