dfb361027ef7a3d5dd200d335570e1c5ffe11aeb
peekoo123 <peekoo123@googlemail.com>
2025-06-16 15:50:59 +0000

    chore: deploy 🚀

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -21,6 +21,16 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
+
+  // Proxy all /api/* calls to your real backend
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`
+      }
+    ]
+  },
 };
 
 module.exports = nextConfig;
