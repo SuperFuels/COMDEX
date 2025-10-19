bc37ce0e518774cc7a3ceaaed288208e9051a4e9
SuperFuels <kevin@super-fuels.com>
2025-10-19 21:19:47 +0000

    Q Series V2

---
+++ frontend/pages/aion/holographic-viewer.tsx
@@ -0,0 +1,6 @@
+import HolographicViewer from "@/components/Hologram/HolographicViewer";
+
+export default function HolographicViewerPage() {
+  // you can pass props if needed (like containerId)
+  return <HolographicViewer containerId="hqce_main_runtime" />;
+}
